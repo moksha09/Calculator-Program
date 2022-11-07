7b4dcf6801fa231b79287ee6c7ff22b4f1e5ecc4
moksha09 <62187683+moksha09@users.noreply.github.com>
2022-11-07 14:33:55 +0530

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -6,14 +6,11 @@ class calculator {
         char operator;
         Double number1, number2, result;
 
-        // create an object of Scanner class
         Scanner input = new Scanner(System.in);
 
-        // ask users to enter operator
         System.out.println("Choose an operator: +, -, *, or /");
         operator = input.next().charAt(0);
 
-        // ask users to enter numbers
         System.out.println("Enter first number");
         number1 = input.nextDouble();
 
